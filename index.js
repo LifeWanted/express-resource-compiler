07125adc0a82177f78d028e4e5247328c6d47743
Oz Wolfe <oz@lifewanted.com>
2012-09-03 11:00:19 -0700

    Adding back entry that aliases the compiler lib

---
+++ index.js
@@ -0,0 +1,8 @@
+
+var compiler = require( './lib/compiler.js' );
+var combiner = require( './lib/combiner.js' );
+
+module.exports = compiler;
+exports.compiler = compiler;
+exports.combiner = combiner;
+g
