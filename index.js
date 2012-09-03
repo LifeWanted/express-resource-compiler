ce62c7315dee7c5c313032a99991581b591502c9
Oz Wolfe <oz@lifewanted.com>
2012-09-03 13:26:05 -0700

    Fixed exports

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 var compiler = require( './lib/compiler.js' );
 var combiner = require( './lib/combiner.js' );
 
+compiler.compiler = compiler;
+compiler.combiner = combiner;
 module.exports = compiler;
-exports.compiler = compiler;
-exports.combiner = combiner;
